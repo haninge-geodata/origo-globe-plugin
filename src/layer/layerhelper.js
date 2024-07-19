48c8373841809470e2d4818daa41ba461587cdb5
Patrik Malvenius <malvenius@gmail.com>
2024-07-19 08:55:54 +0200

    add basic functionality for transparency slider for 3dtiles

--- src/layer/layerhelper.js
+++ src/layer/layerhelper.js
@@ -1,6 +1,12 @@
 import Layer from "ol/layer/Layer";
 import Source from "ol/source/Source";
 import LayerProperty from "ol/layer/Property.js";
+import {
+  Cesium3DTileStyle,
+  ConditionsExpression,
+  Color,
+ 
+} from 'cesium';
 
 
 const superOptions = {
@@ -12,6 +18,8 @@ class ThreedTile extends Layer {
     for (const [key, value] of Object.entries(options)) {
       key === "visible" ? this.set(LayerProperty.VISIBLE, value) : (this.values_[key] = value);
     }
+    this.CesiumTileset = undefined;
+    this.Opacity = 1;
     this.setVisible = function (visible) {
       this.set(LayerProperty.VISIBLE, visible);
       this.CesiumTileset.show = !this.CesiumTileset.show;
@@ -22,6 +30,27 @@ class ThreedTile extends Layer {
     };
     this.getMinResolution = function () {
       return 0;
+    };
+    this.setOpacity = function (alpha) {
+      this.Opacity = alpha;
+      let expr = this.CesiumTileset.style.color.conditionsExpression.conditions;
+
+       const cond = expr.map((c) => {
+        const regex = /'(.*?)'/;
+        const col = regex.exec(c[1])[0];
+        const string =  `color(${col}, ${alpha})` 
+        return [c[0], string]
+      })
+
+      this.CesiumTileset.style = new Cesium3DTileStyle({color: {
+        conditions : cond}});
+    }
+
+    
+    ;
+    this.getOpacity = function () {
+      return this.Opacity;
+
     };
   }
 }
